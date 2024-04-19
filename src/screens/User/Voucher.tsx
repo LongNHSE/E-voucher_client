47d2a5c379bbfb3984d3e0e47f41139cf4973d91
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-04-19 21:38:09 +0700

    voucher list [PhiN]

--- src/screens/User/Voucher.tsx
+++ src/screens/User/Voucher.tsx
@@ -9,8 +9,20 @@ import {
   SearchIcon,
   Center,
   Input,
+  Modal,
+  FormControl,
+  Button,
+  HStack,
+  Select,
+  Divider,
 } from "native-base";
-import { Pressable, StyleSheet } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Touchable,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Constants from "expo-constants";
@@ -18,6 +30,7 @@ import { TextInput } from "react-native-paper";
 import { baseUrl } from "../../utils/appConstant";
 import voucherPlaceholder from "../../../assets/icon.png";
 import { Ionicons } from "@expo/vector-icons";
+import { green100 } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 
 interface Voucher {
   _id: string;
@@ -71,7 +84,7 @@ const Voucher = ({ navigation }: any) => {
   };
 
   //filter
-  const [filter, setFilter] = useState<string[]>([
+  const initFilter: string[] = [
     "Food",
     "Drink",
     "Entertainment",
@@ -82,58 +95,186 @@ const Voucher = ({ navigation }: any) => {
     "Beauty",
     "Service",
     "Other",
-  ]);
+  ];
+  const [filter, setFilter] = useState<string[]>(initFilter);
 
   useEffect(() => {
     fetchVouchers();
   }, [isFocused]);
 
+  //modal
+  const [modalVisible, setModalVisible] = React.useState(false);
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>E-Vouchers</Text>
-        <Input
-          marginTop={3}
-          variant="filled"
-          width="100%"
-          borderRadius="10"
-          py="1"
-          px="2"
-          placeholder="Search..."
-          InputLeftElement={<Ionicons name="search" size={24} color="black" />}
-        />
+        <View style={{ flexDirection: "row" }}>
+          <Input
+            marginTop={3}
+            variant="filled"
+            width="90%"
+            borderRadius="10"
+            py="1"
+            px="2"
+            placeholder="Search..."
+            InputLeftElement={
+              <Ionicons name="search" size={24} color="black" />
+            }
+          />
+
+          {/* modal */}
+          <Modal
+            isOpen={modalVisible}
+            onClose={() => setModalVisible(false)}
+            initialFocusRef={initialRef}
+            finalFocusRef={finalRef}
+          >
+            <Modal.Content>
+              <Modal.CloseButton />
+              <Modal.Header>Filter</Modal.Header>
+              <Modal.Body>
+                <Select
+                  selectedValue={filter[0]}
+                  minWidth={200}
+                  accessibilityLabel="Choose Service"
+                  placeholder="Choose Service"
+                  onValueChange={(itemValue) => {
+                    setFilter([itemValue]);
+                  }}
+                  _selectedItem={{
+                    bg: "cyan.600",
+                    endIcon: <Ionicons name="checkmark" size={4} />,
+                  }}
+                >
+                  {initFilter.map((item, index) => (
+                    <Select.Item label={item} value={item} key={index} />
+                  ))}
+                </Select>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button.Group space={2}>
+                  <Button
+                    variant="ghost"
+                    colorScheme="blueGray"
+                    onPress={() => {
+                      setModalVisible(false);
+                    }}
+                  >
+                    Clear
+                  </Button>
+                  <Button
+                    onPress={() => {
+                      setModalVisible(false);
+                    }}
+                  >
+                    Save
+                  </Button>
+                </Button.Group>
+              </Modal.Footer>
+            </Modal.Content>
+          </Modal>
+          <HStack space={2} alignItems={"center"} justifyContent={"center"}>
+            <Pressable
+              onPress={() => {
+                setModalVisible(true);
+              }}
+              style={({ pressed }) => [pressed ? styles.isPress : null]}
+            >
+              <Ionicons
+                style={{ paddingTop: 10, paddingLeft: 10 }}
+                name="filter-outline"
+                size={28}
+                color="black"
+              />
+            </Pressable>
+          </HStack>
+        </View>
+        {/* end of modal */}
       </View>
 
-      <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-        {filter.map((item) => (
-          <Pressable style={styles.filterItem} onPress={() => {}}>
-            <Text fontSize={"md"}>{item}</Text>
-          </Pressable>
-        ))}
-      </ScrollView>
       <FlatList
+        backgroundColor={"#004165"}
         data={vouchers}
         keyExtractor={(item: Voucher) => "_" + item._id.toString()}
         renderItem={({ item }) => (
-          <Pressable
+          <TouchableOpacity
+            style={[styles.item]}
+            key={item._id}
             onPress={() => {
               // navigation.navigate("VoucherDetail");
             }}
           >
             <View style={styles.item}>
-              <Image
-                source={{
-                  uri: item.imageUrl,
-                }}
-                alt={"(voucher-image)"}
-                width={100}
-                height={100}
-              />
-              <Text>{item.name}</Text>
+              <View style={styles.voucherHeader}>
+                <Image
+                  source={{
+                    uri: item.imageUrl,
+                  }}
+                  alt={"(voucher-image)"}
+                  size={"lg"}
+                />
+                <View flexDirection={"column"} marginLeft={5} marginRight={5}>
+                  <Text fontSize={20} fontWeight={"bold"}>
+                    {item.name}
+                  </Text>
+                  <View flexDirection={"row"} alignItems={"center"}>
+                    <Ionicons
+                      style={{ paddingRight: 10 }}
+                      name="ticket-outline"
+                      color="green"
+                      size={24}
+                    />
+                    <Text fontSize={30} color={"green.800"}>
+                      {item.discount}
+                    </Text>
+                    <Text fontSize={20} color={"green.800"}>
+                      {item.discountType === "percent" ? "%" : "$"}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+
+              <View flexDirection={"row"}>
+                <View
+                  width={50}
+                  height={50}
+                  position={"absolute"}
+                  left={-58}
+                  top={-12}
+                  borderRadius={50}
+                  backgroundColor={"#004165"}
+                />
+                <View
+                  height={0}
+                  width={"98%"}
+                  borderWidth={2}
+                  borderColor={"gray.500"}
+                  borderStyle={"dashed"}
+                  margin={2}
+                />
+                <View
+                  width={50}
+                  height={50}
+                  position={"absolute"}
+                  right={-58}
+                  top={-12}
+                  borderRadius={50}
+                  backgroundColor={"#004165"}
+                />
+              </View>
               <Text>{item.description}</Text>
-              <Text>{item.discount}</Text>
+              <Text>
+                {`Availble in: ${new Date(
+                  item.startUseTime
+                ).toLocaleDateString()} - ${new Date(
+                  item.endSellTime
+                ).toLocaleDateString()}`}
+              </Text>
             </View>
-          </Pressable>
+          </TouchableOpacity>
         )}
       ></FlatList>
     </View>
@@ -148,9 +289,12 @@ const styles = StyleSheet.create({
   },
   item: {
     backgroundColor: "white",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    padding: 10,
+    margin: 10,
+    borderRadius: 10,
+  },
+  voucherHeader: {
+    flexDirection: "row",
   },
   header: {
     backgroundColor: "white",
@@ -162,14 +306,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
-  filterItem: {
-    padding: 10,
-    margin: 10,
-    height: 40,
-    borderRadius: 5,
-    backgroundColor: "lightgray",
-  },
   isPress: {
-    transform: [{ scale: 0.95 }],
+    transform: [{ scale: 0.9 }],
+    opacity: 0.5,
   },
 });
